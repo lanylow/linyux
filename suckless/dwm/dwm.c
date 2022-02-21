a8b5b2a93b6eac049fb351006595851bfbfda87e
lanylow <lanylow@protonmail.com>
2022-02-21 21:24:45 +0100

    Add the multi-monitor patch and change the mod key.

--- suckless/dwm/dwm.c
+++ suckless/dwm/dwm.c
@@ -742,7 +742,7 @@ drawbar(Monitor *m)
 	Client *c;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
@@ -2064,9 +2064,12 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
-	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+  Monitor* m;
+ 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
+ 		strcpy(stext, "dwm-"VERSION);
+
+  for(m = mons; m; m = m->next)
+    drawbar(m);
 }
 
 void
